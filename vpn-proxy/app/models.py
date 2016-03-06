5414856dd452d01697d61d30c89c61908b5f33d9
christos pollalis <christos pollalis>
2016-03-06 13:54:22 +0200

    new model & forwarding functionality

--- vpn-proxy/app/models.py
+++ vpn-proxy/app/models.py
@@ -10,6 +10,7 @@ from django.core.exceptions import ValidationError
 
 from .tunnels import start_tunnel, stop_tunnel, gen_key
 from .tunnels import get_conf, get_client_conf, get_client_script
+from .tunnels import run
 
 
 IFACE_PREFIX = 'vpn-proxy-tun'
@@ -20,30 +21,42 @@ log = logging.getLogger(__name__)
 
 
 def chose_server_ip(network=VPN_ADDRESSES):
-    """Find an available server IP in the given network (CIDR notation)"""
-    network = netaddr.IPNetwork(network)
-    if network.version != 4 or not network.is_private():
-        raise Exception("Only private IPv4 networks are supported.")
-    first, last = network.first, network.last
-    if first % 2:
-        first += 1
-    for i in range(20):
-        addr = str(netaddr.IPAddress(random.randrange(first, last, 2)))
-        print addr
-        try:
-            Tunnel.objects.get(server=addr)
-        except Tunnel.DoesNotExist:
-            return addr
+	"""Find an available server IP in the given network (CIDR notation)"""
+	network = netaddr.IPNetwork(network)
+	if network.version != 4 or not network.is_private():
+		raise Exception("Only private IPv4 networks are supported.")
+	first, last = network.first, network.last
+	if first % 2:
+		first += 1
+	for i in range(20):
+		addr = str(netaddr.IPAddress(random.randrange(first, last, 2)))
+		print addr
+		try:
+			Tunnel.objects.get(server=addr)
+		except Tunnel.DoesNotExist:
+			return addr
 
 
 def check_server_ip(addr):
-    """Verify that the server IP is valid"""
-    addr = netaddr.IPAddress(addr)
-    if addr.version != 4 or not addr.is_private():
-        raise ValidationError("Only private IPv4 networks are supported.")
-    if not 0 < addr.words[-1] < 254 or addr.words[-1] % 2:
-        raise ValidationError("Server IP's last octet must be even in the "
-                              "range [2,254].")
+	"""Verify that the server IP is valid"""
+	addr = netaddr.IPAddress(addr)
+	if addr.version != 4 or not addr.is_private():
+		raise ValidationError("Only private IPv4 networks are supported.")
+	if not 0 < addr.words[-1] < 254 or addr.words[-1] % 2:
+		raise ValidationError("Server IP's last octet must be even in the "
+		                      "range [2,254].")
+
+
+def pick_port(_port):
+	"""Find next available port based on Ports. This function is used directly by views.py"""
+	print('Checking port availability.')
+	for _ in range(100):
+		try:
+			Ports.objects.get(loc_port=_port)
+			_port += 1
+		except Ports.DoesNotExist:
+			print('Available port: ' + str(_port))
+			return _port
 
 
 class Tunnel(models.Model):
@@ -135,3 +148,67 @@ class Tunnel(models.Model):
     def delete(self, *args, **kwargs):
         self.stop()
         super(Tunnel, self).delete(*args, **kwargs)
+
+
+class Ports(models.Model):
+	# src_addr = models.GenericIPAddressField(protocol='IPv4')
+	dst_addr = models.GenericIPAddressField(protocol='IPv4')
+	dst_port = models.IntegerField()
+	tunel_id = models.IntegerField()
+	loc_port = models.IntegerField(unique=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	@property
+	def tunnel(self):
+		return '%s%s' % (IFACE_PREFIX, self.tunel_id)
+
+	@property
+	def port(self):
+		return self.loc_port
+
+	def __str__(self):
+		return 'Local port %s via %s -> %s:%s' % (self.port, self.tunnel, self.dst_addr, self.dst_port)
+
+	def to_dict(self):
+		return {
+			'id': self.id,
+			# 'src_addr': self.src_addr,
+		    'dst_addr': self.dst_addr,
+			'dst_port': self.dst_port,
+			'tunnel_name': self.tunnel,
+			'loc_port': self.loc_port
+		}
+
+	def forward(self, mode, **new_record):
+		modes = {
+			'add': '-A',
+			'delete': '-D'
+		}
+		for key in modes.iterkeys():
+			if key == mode:
+				job = modes[key]
+				mark = self.tunel_id
+				# DNAT incoming packets to the specified local port --> private host (IP, PORT)
+				run(['iptables', '-t', 'nat', job, 'PREROUTING', '--dport', str(new_record['loc_port']), '-j', 'DNAT',
+				     '--to', str(new_record['dst_addr'] + ':' + str(new_record['dst_port']))])
+				# MASQUERADE packets routed via the virtual interface
+				run(['iptables', '-t', 'nat', job, 'POSTROUTING', '-o', str(new_record['tunnel_name']), '-j', 'MASQUERADE'])
+				# mangle incoming packets
+				run(['iptables', '-t', 'mangle', job, 'INPUT', '--dport', str(new_record['loc_port']), '-j', 'MARK',
+				     '--set-mark', str(mark)])
+				# point marked packets to the corresponding routing table as created during `OPENVPN start`
+				run(['iprule', str(key), 'fwmark', str(mark), 'table', str(mark)])
+
+	def save(self, *args, **kwargs):
+		"""Insert forwarding rules as soon as entry is saved"""
+		self.full_clean()
+		super(Ports, self).save(*args, **kwargs)
+		new_record = self.to_dict()
+		self.forward('add', **new_record)
+		return self.id
+
+	def delete(self, *args, **kwargs):
+		"""Delete forwarding rules along entry removal"""
+		old_record = self.to_dict()
+		self.forward('delete', **old_record)
+		super(Ports, self).delete(*args, **kwargs)
--- vpn-proxy/app/views.py
+++ vpn-proxy/app/views.py
@@ -3,37 +3,54 @@ from django.http import JsonResponse as _JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 
-from .models import Tunnel
+from .models import Tunnel, Ports, pick_port
+
+from netaddr import IPAddress
 
 
 class JsonResponse(_JsonResponse):
-    def __init__(self, data, **kwargs):
-        kwargs['safe'] = False
-        kwargs['json_dumps_params'] = {'indent': 4}
-        super(JsonResponse, self).__init__(data, **kwargs)
+	def __init__(self, data, **kwargs):  # data {}
+		kwargs['safe'] = False           # any object can be passed for serialization, not only dict
+		kwargs['json_dumps_params'] = {'indent': 4}
+		super(JsonResponse, self).__init__(data, **kwargs)
 
 
 @require_http_methods(['GET', 'POST'])
 def tunnels(request):
-    if request.method == 'POST':
-        params = {}
-        if 'server' in request.POST:
-            params['server'] = request.POST['server']
-        tun = Tunnel(**params)
-        tun.save()
-        return JsonResponse(tun.to_dict())
-    return JsonResponse(map(Tunnel.to_dict, Tunnel.objects.all()))
+	if request.method == 'POST':
+		params = {}
+		if 'server' in request.POST:
+			params['server'] = request.POST['server']
+		tun = Tunnel(**params)
+		tun.save()
+		return JsonResponse(tun.to_dict())
+	return JsonResponse(map(Tunnel.to_dict, Tunnel.objects.all()))
 
 
 @require_http_methods(['GET', 'POST'])
 def tunnel(request, tunel_id):
-    tun = get_object_or_404(Tunnel, pk=tunel_id)
-    if request.method == 'POST':
-        tun.start()
-    return JsonResponse(tun.to_dict())
+	tun = get_object_or_404(Tunnel, pk=tunel_id)
+	if request.method == 'POST':
+		tun.start()
+	return JsonResponse(tun.to_dict())
 
 
 @require_http_methods(['GET'])
 def script(request, tunel_id):
-    tun = get_object_or_404(Tunnel, pk=tunel_id)
-    return HttpResponse(tun.client_script)
+	tun = get_object_or_404(Tunnel, pk=tunel_id)
+	return HttpResponse(tun.client_script)
+
+
+@require_http_methods(['GET'])
+def connection(request, peer, target, port, tunel_id):
+	_port = int(port) + 5000 + int(tunel_id)
+	entry = {}
+	# entry['src_addr'] = IPAddress(peer)
+	entry['dst_addr'] = IPAddress(target)
+	entry['dst_port'] = int(port)
+	entry['tunel_id'] = tunel_id
+	entry['loc_port'] = pick_port(_port)
+	assoc = Ports(**entry)
+	with assoc.save() as _id:
+		new_entry = get_object_or_404(Ports, pk=_id)
+		return HttpResponse(new_entry.port)
