dff9ab05d0d3f7f2a8f2e69d1d1e743966dfce51
Dimitris Rozakis <dimrozakis@gmail.com>
2016-07-15 22:10:48 +0300

    Add reset tunnels management command

---
+++ vpn-proxy/app/management/commands/reset_tunnels.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from app.models import Tunnel
+
+
+class Command(BaseCommand):
+    help = "Create superuser if missing (Non Interactive)."
+
+    def add_arguments(self, parser):
+        parser.add_argument('tunnel', nargs='*', type=int)
+
+    def handle(self, *args, **kwargs):
+        if kwargs['tunnel']:
+            tunnels = Tunnel.objects.filter(id__in=kwargs['tunnel'])
+        else:
+            tunnels = Tunnel.objects.all()
+        for tunnel in tunnels:
+            self.stdout.write("Resetting tunnel %d..." % tunnel.id)
+            tunnel.reset()
